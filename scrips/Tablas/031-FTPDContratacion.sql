f4e2a3d943add023366541ed9028e29485f0ecf3
washan <jmadrigal19@gmail.com>
2014-09-27 12:31:51 -0600

    Nuevas tablas de contratos

---
+++ scrips/Tablas/031-FTPDContratacion.sql
@@ -0,0 +1,34 @@
+/*==============================================================*/
+/* Table: FTPDContratacion                                      */
+/*==============================================================*/
+create table FTPDContratacion (
+   PCid                 numeric                        not null,
+   PCDid                numeric                        identity,
+   SDid                 numeric                        not null,
+   DVLcodigo            char(10)                       null,
+   DVid                 numeric                        null,
+   PCDValor             varchar(1024)                  null,
+   constraint FTPDContratacion_PK primary key (PCDid),
+   constraint FTPDContratacion_UK unique (PCid, SDid)
+)
+go
+
+alter table FTPDContratacion
+   add constraint FK_FTPDCONT_REFERENCE_FTPCONTR foreign key (PCid)
+      references FTPContratacion (Id_del_proceso_e_contratacion)
+go
+
+alter table FTPDContratacion
+   add constraint FK_FTPDCONT_REFERENCE_FTSECCIO foreign key (SDid)
+      references FTSeccionesD (SDid)
+go
+
+alter table FTPDContratacion
+   add constraint FK_FTPDCONT_REFERENCE_FTDATOSV foreign key (DVid)
+      references FTDatosVariables (DVid)
+go
+
+alter table FTPDContratacion
+   add constraint FK_FTPDCONT_REFERENCE_FTLISTAV foreign key (DVid, DVLcodigo)
+      references FTListaValores (DVid, DVLcodigo)
+go

a553bb7d23534ba084abf0b53fde5a9ff7974079
ljimenezcrc <luisejimenezs@gmail.com>
2014-10-17 11:43:31 -0600

    actaulizacion de campos nuevos

--- scrips/Tablas/031-alters campos nuevos.sql
+++ scrips/Tablas/031-alters campos nuevos.sql
@@ -10,7 +10,16 @@ alter table FTHistoriaTramite
       references FTPContratacion (PCid)
 go
 
-
 alter table FTPContratacion add PCEstadoCivil integer null
 go
 alter table FTPContratacion add  constraint CKC_PCESTADOCIVIL_FTPCONTR check (PCEstadoCivil is null or (PCEstadoCivil in (1,2,3,4,5)))
+
+
+/*05:16 p.m. 07/10/2014*/
+
+alter table FTContratos add  TTid numeric(18) null
+go
+alter table FTContratos
+   add constraint FK_FTCONTRA_REFERENCE_FTTIPOTR foreign key (TTid)
+      references FTTipoTramite (TTid)
+go
